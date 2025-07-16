622c958c5d5fea0f0b8328c51b27d50b6785a924
Golu kr <prajaptigolu07@gmail.com>
2025-07-16 21:02:51 +0530

    Initial commit - DocBook project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))',
+      },
+      colors: {
+        primary: '#5F6FFF',
+      },
+    },
+  },
+  plugins: [],
+}
